7b2298cf03d42202cb367c4bf2273b708dbe3b9f
skmbw <tongku2008@126.com>
2015-04-30 16:22:37 +0800

    表单数据绑定

--- restful/src/com/vteba/service/restful/provider/DefaultFormProvider.java
+++ restful/src/com/vteba/service/restful/provider/DefaultFormProvider.java
@@ -20,6 +20,7 @@ import org.apache.commons.io.IOUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.vteba.user.model.User;
+import com.vteba.utils.charstr.StringUtils;
 
 /**
  * 表单类型的提交的数据解析器。接受表单post提交。返回json数据。
@@ -98,4 +99,56 @@ public class DefaultFormProvider implements MessageBodyReader<Object>, MessageBo
 		return user;
 	}
 
+	public static String buildJson(String content) {
+		if (StringUtils.hasText(content)) {
+			StringBuilder json = new StringBuilder("{");
+			boolean append = false;
+			String[] forms = content.split("&");
+			if (forms != null) {
+				for (String form : forms) {
+					String[] args = form.split("=");
+					if (args != null && args.length == 2) {
+						String key = args[0];
+						String value = args[1];
+						if (key != null && value != null) {
+							if (key.indexOf(".") > -1) {// 含有多级属性
+								
+							} else {
+							}
+							if (!append) {
+								json.append("\"").append(key).append("\":\"").append(value).append("\"");
+								append = true;
+							} else {
+								json.append(",\"").append(key).append("\":\"").append(value).append("\"");
+							}
+						}
+					}
+				}
+			}
+			return json.append("}").toString();
+		}
+		return null;
+	}
+	
+	public static String buildJson(InputStream is) {
+		try {
+			String content = IOUtils.toString(is);
+			return buildJson(content);
+		} catch (IOException e) {
+			
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		StringBuilder text = new StringBuilder();
+		text.append("{\"").append("id\":\"").append("2\",").append("\"name\":\"yinlei\"}");
+		User user = JSON.parseObject(text.toString(), User.class);
+		System.out.println(user);
+		long d = System.currentTimeMillis();
+		String content = "id=22&name=yinlei&createDate=2015-04-30 14:22:17";
+		content = buildJson(content);
+		user = JSON.parseObject(content, User.class);
+		System.out.println(System.currentTimeMillis() - d);
+	}
 }
